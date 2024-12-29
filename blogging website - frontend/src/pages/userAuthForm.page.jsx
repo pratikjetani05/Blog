cfdf6c3c34a5cc3c5720f14c353ed60cd4172799
pratikjetani05 <pratikjetani14@gmail.com>
2024-12-29 13:15:37 +0530

    Initial commit

---
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -0,0 +1,185 @@
+
+
+import { Link, Navigate } from "react-router-dom";
+import { useContext, useRef } from "react";
+import InputBox from "../components/input.component";
+import googleicon from "../imgs/google.png";
+import AnimationWrapper from "../common/page-animation";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+import { storeInSession } from "../common/session";
+import { UserContext } from "../App";
+import { authWithGoogle } from "../common/firebase";
+
+const UserAuthForm = ({ type }) => {
+  let {
+    userAuth: { access_token },
+    setUserAuth,
+  } = useContext(UserContext);
+
+  const userAuthThroughServer = (serverRoute, fromData) => {
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, fromData)
+      .then(({ data }) => {
+        storeInSession("user", JSON.stringify(data));
+
+        setUserAuth(data);
+      })
+      .catch(({ response }) => {
+        toast.error(response.data.error);
+      });
+  };
+  // const userAuthThroughServer = async (serverRoute, formData) => {
+  //   try {
+  //     const { data } = await axios.post(${import.meta.env.VITE_SERVER_DOMAIN}${serverRoute}, formData);
+  //     storeInSession('user', JSON.stringify(data));
+  //     setUserAuth(data);
+  //   } catch (error) {
+  //     toast.error(error.response?.data?.error || 'An error occurred. Try again.');
+  //   }
+  // };
+
+  const formRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+
+    let emailRegex = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+    //formData
+    let form = new FormData(formRef.current);
+    let formData = {};
+
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
+    }
+
+    let { fullname, email, password } = formData;
+
+    //form validation
+    if (fullname) {
+      if (fullname.length < 3) {
+        return toast.error("Fullname must be atleast 3 letters long");
+      }
+    }
+    if (!email.length) {
+      return toast.error("Enter Email");
+    }
+    if (!emailRegex.test(email)) {
+      return toast.error("Email is invalid");
+    }
+    if (!passwordRegex.test(password)) {
+      return toast.error(
+        "Password should be 6 to 20 character long with numeric, 1 lowercase and 1 uppercase letters"
+      );
+    }
+
+    userAuthThroughServer(serverRoute, formData);
+  };
+
+  const handleGoogleAuth = (e) => {
+    e.preventDefault();
+
+    authWithGoogle()
+      .then(async (user) => {
+        if (!user) throw new Error("User not authenticated");
+
+        const serverRoute = "/google-auth";
+        const formData = { access_token: user.accessToken };
+
+        try {
+          await userAuthThroughServer(serverRoute, formData);
+        } catch (serverError) {
+          toast.error("Server authentication failed. Please try again.");
+          console.error("Server Auth Error:", serverError);
+        }
+      })
+      .catch((err) => {
+        toast.error(
+          "Trouble logging in through Google. Please try again later."
+        );
+        console.error("Google Login Error:", err);
+      });
+  };
+
+  return access_token ? (
+    <Navigate to="/" />
+  ) : (
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex items-center justify-center ">
+        <Toaster />
+        <form id="formElement" ref={formRef} className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type == "sign-in" ? "Welcome Back" : "Join us today"}
+          </h1>
+          {type != "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Full Name"
+              icon="fi-rr-user"
+            />
+          ) : (
+            ""
+          )}
+          <InputBox
+            name="email"
+            type="email"
+            placeholder="E-mail"
+            icon="fi-rr-envelope"
+          />
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Password"
+            icon="fi-rr-key"
+          />
+
+          <button
+            className="btn-dark center mt-14"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            {type.replace("-", " ")}
+          </button>
+
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <p>or</p>
+            <hr className="w-1/2 border-black" />
+          </div>
+
+          <button
+            type="button"
+            className="btn-dark flex items-center justify-center gap-4 w-[90%]  center"
+            onClick={handleGoogleAuth}
+          >
+            <img src={googleicon} alt="icon of google" className="w-5" />
+            contine with google
+          </button>
+
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Don't have an account?
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+                Join us today
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Already a member
+              <Link to="/signin" className="underline text-black text-xl ml-1">
+                Sign in here.
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </AnimationWrapper>
+  );
+};
+
+export default UserAuthForm;
