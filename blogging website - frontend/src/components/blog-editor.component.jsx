1c2c7af458205a2279a3591027cd245122ca135c
pratikjetani05 <pratikjetani14@gmail.com>
2025-01-01 22:04:30 +0530

    phase 2 done

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -1,28 +1,46 @@
 import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
 import { uploadImage } from "../common/aws";
 import { Toaster, toast } from "react-hot-toast";
 import { EditorContext } from "../pages/editor.pages";
-import EditorJS  from "@editorjs/editorjs";
+import EditorJS from "@editorjs/editorjs";
 import { tools } from "./tools.component";
+import axios from "axios";
+import { UserContext } from "../App";
 
 function BlogEditor() {
-  // let blogBannerRef = useRef();  
-  let { blog , blog: {title , banner , content , tags , des}, setBlog, textEditor , setTextEditor, setEditorState} = useContext(EditorContext)
+  // let blogBannerRef = useRef();
+  let {
+    blog,
+    blog: { title, banner, content, tags, des },
+    setBlog,
+    textEditor,
+    setTextEditor,
+    setEditorState,
+  } = useContext(EditorContext);
+
+  let {
+    userAuth: { access_token },
+  } = useContext(UserContext);
+
+  let navigate = useNavigate();
 
   //useEffect
   useEffect(() => {
-    setTextEditor(new EditorJS({
-      holderId: "textEditor",
-      data: content,
-      tools: tools,
-      placeholder: "Write your blog here"
-    }));
+    if (!textEditor.isReady) {
+      setTextEditor(
+        new EditorJS({
+          holderId: "textEditor",
+          data: content,
+          tools: tools,
+          placeholder: "Write your blog here",
+        })
+      );
+    }
   }, []);
-  
 
   const handleBannerUpload = (e) => {
     let img = e.target.files[0];
@@ -37,7 +55,7 @@ function BlogEditor() {
             toast.success("uploaded successfully");
             // blogBannerRef.current.src = url;
 
-            setBlog({...blog, banner: url});
+            setBlog({ ...blog, banner: url });
           }
         })
         .catch((err) => {
@@ -48,49 +66,101 @@ function BlogEditor() {
   };
 
   const handleTitleKeyDown = (e) => {
-    if(e.keyCode === 13){
+    if (e.keyCode === 13) {
       e.preventDefault();
     }
-  }
+  };
 
   const handleTitleChange = (e) => {
     let input = e.target;
-    
+
     input.style.height = "auto";
-    input.style.height = input.scrollHeight + "px"  ;
+    input.style.height = input.scrollHeight + "px";
 
     setBlog({ ...blog, title: input.value });
-  }
+  };
 
   const handleError = (e) => {
-    let img =  e.target;
+    let img = e.target;
 
     img.src = defaultBanner;
-  }
+  };
 
   const handlePublishEvent = () => {
-      if(!banner.length){
-        return toast.error("Upload a blog banner to publish it");
-      }
+    if (!banner.length) {
+      return toast.error("Upload a blog banner to publish it");
+    }
 
-      if(!title.length){
-        return toast.error("Enter a blog title to publish it");
-      }
+    if (!title.length) {
+      return toast.error("Enter a blog title to publish it");
+    }
 
-      if(textEditor.isReady){
-        textEditor.save().then(data => {
-          if(data.blocks.length){
-            setBlog({...blog, content: data});
+    if (textEditor.isReady) {
+      textEditor
+        .save()
+        .then((data) => {
+          if (data.blocks.length) {
+            setBlog({ ...blog, content: data });
             setEditorState("publish");
-          }else{
+          } else {
             return toast.error("Enter a blog content to publish it");
           }
         })
         .catch((err) => {
           console.log(err);
-        })
-      }
-  }
+        });
+    }
+  };
+
+  const handleSaveDraft = (e) => {
+    if (e.target.className.includes("disable")) {
+      return;
+    }
+
+    if (!title.length) {
+      return toast.error("Writr blog title before saving it as a draft");
+    }
+
+    let loadingToast = toast.loading("Saveing Draft...");
+
+    e.target.classList.add("disable");
+
+    if (textEditor.isReady) {
+      textEditor.save().then((content) => {
+        let blogObj = {
+          title,
+          banner,
+          des,
+          content,
+          tags,
+          draft: true,
+        };
+
+        axios
+          .post(import.meta.env.VITE_SERVER_DOMAIN + "/create-blog", blogObj, {
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+            },
+          })
+          .then(() => {
+            e.target.classList.add("disable");
+
+            toast.dismiss(loadingToast);
+            toast.success("Saved👍");
+
+            setTimeout(() => {
+              navigate("/");
+            }, 500);
+          })
+          .catch(({ response }) => {
+            e.target.classList.remove("disable");
+            toast.dismiss(loadingToast);
+
+            return toast.error(response.data.error);
+          });
+      });
+    }
+  };
 
   return (
     <>
@@ -99,16 +169,16 @@ function BlogEditor() {
           <img src={logo} />
         </Link>
         <p className="max-md:hidden text-black line line-clamp-1 w-full ">
-          { title.length ? title : "New blog"}
+          {title.length ? title : "New blog"}
         </p>
 
         <div className="flex gap-4 ml-auto">
-          <button className="btn-dark py-2"
-              onClick={handlePublishEvent}
-          >
+          <button className="btn-dark py-2" onClick={handlePublishEvent}>
             Publish
           </button>
-          <button className="btn-light py-2">Save Draft</button>
+          <button className="btn-light py-2" onClick={handleSaveDraft}>
+            Save Draft
+          </button>
         </div>
       </nav>
 
@@ -142,15 +212,11 @@ function BlogEditor() {
               className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
               onKeyDown={handleTitleKeyDown}
               onChange={handleTitleChange}
-            >
+            ></textarea>
 
-            </textarea>
+            <hr className="w-full opacity-10 my-5" />
 
-            <hr className="w-full opacity-10 my-5"/>
-
-            <div id="textEditor" className="font-gelasio">
-            </div>
-
+            <div id="textEditor" className="font-gelasio"></div>
           </div>
         </section>
       </AnimationWrapper>
--- server/server.js
+++ server/server.js
@@ -247,31 +247,29 @@ server.post("/create-blog", verifyJWT, (req, res) => {
   let { title, des, banner, tags, content, draft } = req.body;
 
   if (!title.length) {
-    return res
-      .status(403)
-      .json({ error: "Title is required to publish the blog" });
+    return res.status(403).json({ error: "You must provide a title" });
   }
 
-  if (!des.length || des.length > 200) {
-    return res
-      .status(403)
-      .json({
+  if (!draft) {
+    if (!des.length || des.length > 200) {
+      return res.status(403).json({
         error: "You must provide blog Description under 200 characters",
       });
-  }
+    }
 
-  if (!banner.length) {
-    return res.status(403).json({ error: "You must upload a blog banner" });
-  }
+    if (!banner.length) {
+      return res.status(403).json({ error: "You must upload a blog banner" });
+    }
 
-  if (!content.blocks.length) {
-    return res
-      .status(403)
-      .json({ error: "There must be some blog content to publish it" });
-  }
+    if (!content.blocks.length) {
+      return res
+        .status(403)
+        .json({ error: "There must be some blog content to publish it" });
+    }
 
-  if (!tags.length || tags.length > 10) {
-    return res.status(403).json({ error: "You must provide blog tags" });
+    if (!tags.length || tags.length > 10) {
+      return res.status(403).json({ error: "You must provide blog tags" });
+    }
   }
 
   tags = tags.map((tag) => tag.toLowerCase());
